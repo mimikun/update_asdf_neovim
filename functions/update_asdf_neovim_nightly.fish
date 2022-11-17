f2752fa901935a046378146b967f471a9a2a8e04
Yuto Tanaka <karaage_nennen@icloud.com>
2022-11-17 19:23:53 +0900

    Improved the process of getting the latest nightly version

--- functions/update_asdf_neovim_nightly.fish
+++ functions/update_asdf_neovim_nightly.fish
@@ -1,6 +1,6 @@
 function update_asdf_neovim_nightly --description 'Update asdf-neovim nightly'
     set OLD_VERSION (nvim --version | head -n 1 | string split ' ' | sed -n '2p')
-    set VERSION (curl --silent https://api.github.com/repos/neovim/neovim/releases/tags/nightly | jq .body | string split ' ' | string split '\n' | sed -n '3p')
+    set VERSION (curl --silent https://api.github.com/repos/neovim/neovim/releases/tags/nightly | jq .body | string split ' ' | string split '\n' | sed -n '3p' | sed -e "s/%0ABuild//g")
     if test $OLD_VERSION != $VERSION
         asdf uninstall neovim nightly
         asdf install neovim nightly

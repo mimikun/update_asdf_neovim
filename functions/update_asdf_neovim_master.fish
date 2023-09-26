a6cd1d5d39e9bdab9f30d5584a04f7fb97d0ef88
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-26 16:47:29 +0900

    Remove unused fish scripts

--- functions/update_asdf_neovim_master.fish
+++
@@ -1,5 +0,0 @@
-function update_asdf_neovim_master --description 'Update asdf-neovim master'
-    echo "Update neovim (latest)master!"
-    asdf uninstall neovim ref:master
-    asdf install neovim ref:master
-end
